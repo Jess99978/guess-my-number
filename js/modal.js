36d0dae08b6c7e8512e06a6367f9153251f8f5d8
Jess <candylee022365@gmail.com>
2024-06-15 23:29:30 +0800

    改路徑

--- js/modal.js
+++ js/modal.js
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const btnClose = document.querySelector(".svg__close");
   const modalRule = document.querySelector(".rule");
   const overlay = document.querySelector(".overlay");
-  const clickSound2 = new Audio("../audio/powerup01.mp3");
+  const clickSound2 = new Audio("../powerup01.mp3");
   const closeModal = function () {
     modalRule.classList.add("hidden");
     overlay.classList.add("hidden");
